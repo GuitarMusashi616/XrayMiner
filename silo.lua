cdfeca33dfb33b0c4a9a55e3e94a9b26c5394d1b
= <=>
2021-12-02 21:37:13 -0900

    s

---
+++ silo.lua
@@ -0,0 +1,80 @@
+local function all(tbl) 
+  local prev_k = nil
+  return function()
+    local k,v = next(tbl, prev_k)
+    prev_k = k
+    return v
+  end
+end
+
+local function inc_tbl(tbl, key, val)
+  assert(key, "key cannot be false or nil")
+  val = val or 1
+  if not tbl[key] then
+    tbl[key] = 0
+  end
+  tbl[key] = tbl[key] + val
+end
+
+silo = {
+  dict = {},
+  chest_names = {},
+}
+
+local function beginsWith(string, beginning)
+  return string:sub(1,#beginning) == beginning
+end
+
+local function forEach(tbl, func)
+  for val in all(tbl) do
+    func(val)
+  end
+end
+
+function t2f(tbl, filename)
+  filename = filename or "output"
+  local h = io.open(filename, "w")
+  h:write(textutils.serialize(tbl))
+  h:close()
+  shell.run("edit "..tostring(filename))
+end
+
+-- scan through all connected chests and add to table
+function silo.find_chests()
+  for name in all(peripheral.getNames()) do
+    if beginsWith(name, "minecraft:chest") then
+      table.insert(silo.chest_names, name)
+    end
+  end
+end
+
+-- add the item to the record
+function silo.add(item)
+  inc_tbl(silo.dict, item.name, item.count)
+end
+
+-- scan through all invos and put into dict
+function silo.update_all_items()
+  for name in all(silo.chest_names) do
+    local items = peripheral.call(name, "list")
+    forEach(items, function(item) silo.add(item) end)
+  end
+end
+
+function test_begins_with()
+  local tbl = {"bottom", "minecraft:chest_7", "top", "right", "minecraft:chest_0", "left"}
+  for name in all(tbl) do
+    if beginsWith(name, "minecraft:chest") then
+      print(name)
+    end
+  end
+end
+
+function silo.startup()
+  silo.find_chests()
+  silo.update_all_items()
+end
+
+
+silo.startup()
+t2f(silo.dict)
