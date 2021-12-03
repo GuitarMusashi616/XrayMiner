f3b6577398db263a9bf356486207c350ed9d96e4
= <=>
2021-12-02 19:42:56 -0900

    lets see it work

--- storage.lua
+++ storage.lua
@@ -1,5 +1,5 @@
-local SIDES = {"top", "bottom", "left", "right", "front", "back"}
--- require("peripheral")
+local SIDES = {"top", "left", "right", "front", "back"}
+require("peripheral")
 
 local function all(tbl) 
   local prev_k = nil
@@ -38,10 +38,15 @@ function storage.startup()
   for i=1,storage.max_height do
     storage.scan_at_height(i)
   end
+  storage.go_to_floor()
+end
+
+function storage.make_key(height, side)
+  return tostring(height) .. tostring(side)
 end
 
 function storage.add_to_chests(item, height, side)
-  local key = tostring(height) + tostring(side)
+  local key = storage.make_key(height, side)
   if not storage.chests[key] then
     storage.chests[key] = {}
   end
@@ -50,6 +55,10 @@ end
 
 function storage.add_to_dict(item)
   inc_tbl(storage.dict, item.name, item.count)
+end
+
+function storage.rem_from_dict(name, count)
+  inc_tbl(storage.dict, name, count*-1)
 end
 
 function storage.add(item, height, side)
@@ -66,7 +75,41 @@ function storage.go_to_height(height)
     turtle.down()
     storage.height = storage.height - 1
   end
-  
+end
+
+function storage.find(func, height, side)
+  local key = storage.make_key(height, side)
+  for item in all(storage.chests[key]) do
+    if func(item) then
+      return item
+    end
+  end
+end
+
+function storage.collect(item_name, count, height, side)
+  storage.go_to_height(height)
+  local rem = count
+  local items = peripheral.call(side, "list")
+  for i,item in pairs(items) do
+    if item.name == item_name then
+      local amount_to_take = math.min(count, 64)
+      peripheral.call("bottom", "pullItems", side, amount_to_take)
+      rem = rem - amount_to_take
+    end
+    if rem <= 0 then
+      break
+    end
+  end
+end
+
+function storage.retrieve(item_name, count)
+  storage.go_to_floor()
+  local rem_requested = count
+  for i=1,storage.max_height do
+    for side in all(SIDES) do
+      storage.collect(item_name, count, height, side)
+    end
+  end
 end
 
 function storage.scan_at_height(height)
