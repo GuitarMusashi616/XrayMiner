d7df77c6a7e35f97912623ef72ebf43b6e43d671
= <=>
2021-12-03 18:53:32 -0900

    fixed everything finally

--- xray_old.lua
+++ xray_old.lua
@@ -4,61 +4,45 @@
 require("turtle")
 
 -- mines blocks whose names have these following strings in them
-local TARGETS = {"diamond"}
+local TARGET = "diamond"
 local DEFAULT_RADIUS = 8
 local GEOSCAN_SLOT = 16
 
 
 function main(tArgs)
   if #tArgs==0 then
-    print("Usage: xray <dir> [dist]")
-    print("eg. xray north 5")
+    print("Usage: xray <dir> [x] [y] [z]")
+    print("eg. xray north 0 0 0")
   end
 
   local dir = tArgs[1]
-  local x = 0
-  local y = 0
-  local z = 0
-  local dist = tArgs[2] or 1
+  local x = tArgs[2] or 0
+  local y = tArgs[3] or 0
+  local z = tArgs[4] or 0
 
-  turtle.reset(x,y,z,dir)
-  for _=1,dist do
-    tunnel()
-  end
+  turtle.reset(tArgs[2], tArgs[3], tArgs[4], tArgs[1])
+  tunnel()
+  
 end
-
-function is_target(ore_name)
-  for i,target in pairs(TARGETS) do
-    if ore_name:find(target) then
-      return true
-    end
-  end
-  return false
-end
-
 
 function tunnel()
-  turtle.digUp()
-  for i=1,17 do
-    turtle.dig()
-    turtle.forward()
-    turtle.digUp()
+  for _=1,9 do
+    turtle.forward_or_dig()
   end
   local tbl = scanBlocks()
   local dir = turtle.dir
   turtle.reset(0,0,0,dir)
   for i,item in pairs(tbl) do
-    if is_target(item.name) then
-      turtle.goTo(item.x,item.y,item.z)
+    if item.name:find(TARGET) then
+      turtle.go(item.x,item.y,item.z)
     end
   end
-  turtle.goTo(0,0,0)
-  turtle.turnTo(dir)
+  turtle.go(0,0,0)
 end
 
 function scanBlocks()
   turtle.select(GEOSCAN_SLOT)
-  turtle.up_or_dig()
+  turtle.up()
   turtle.placeDown()
   local tbl = peripheral.call("bottom","scan", DEFAULT_RADIUS)
   turtle.digDown()
