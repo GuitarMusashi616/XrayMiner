26ca2e74fb9976a4d401f5236a34f5e294c4518d
= <=>
2021-12-04 10:47:07 -0900

    reorganized files

--- peripheral.lua
+++
@@ -1,9 +0,0 @@
-peripheral = {}
-peripheral.call = function(a, b) print("peripheral.call(" .. tostring(a) .. ", " .. tostring(b) ..")") return {name="lemon", count=5} end
-peripheral.getType = function(a) return "minecraft:chest" end
-
-local height = 3
-turtle = {}
-turtle.detectDown = function() print("turtle.detectDown()") return height == 1 end
-turtle.up = function() print("turtle.up()") height = height + 1 end
-turtle.down = function() print("turtle.down()") height = height - 1 end
